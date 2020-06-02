4eb84b0e85c1ea5aac971f4e9f0f5ea491e38253
Sam Roxanne <lunar_sam@protonmail.com>
2020-06-02 12:27:53 -0500

    version bump

--- src/luacomp_vars.lua
+++ src/luacomp_vars.lua
@@ -6,6 +6,6 @@ end
 
 _sv("LUACOMP_V_MAJ", 1)
 _sv("LUACOMP_V_MIN", 2)
-_sv("LUACOMP_V_PAT", 1)
+_sv("LUACOMP_V_PAT", 2)
 _sv("LUACOMP_VERSION", LUACOMP_V_MAJ.."."..LUACOMP_V_MIN.."."..LUACOMP_V_PAT)
 _sv("LUACOMP_NAME", "LuaComp")
