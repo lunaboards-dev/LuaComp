5b8a90da8718dac1c8f49518a2b261888dc710a4
sam <30084950+lunaboards-dev@users.noreply.github.com>
2022-02-14 12:14:17 -0500

    nice

--- src/directives/pragma.lua
+++ src/directives/pragma.lua
@@ -2,6 +2,14 @@
 -- License, v. 2.0. If a copy of the MPL was not distributed with this
 -- file, You can obtain one at https://mozilla.org/MPL/2.0/.
 
+local pragma_hooks = {
+	include_once = function(env, value)
+		if value and value ~= 0 then
+
+		end
+	end
+}
+
 function directives.pragma(env, key, value)
 	if not env.pragmas[key] then
 		return nil, "unknown pragma "..key
